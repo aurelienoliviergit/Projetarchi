6826b9d1809a767069669beee6e3e034384cfbb4
ArmandMellet <armand.mellet@hotmail.fr>
2016-11-20 20:17:08 +0100

    commit de Armand

--- Archilog/src/Diagramme.java
+++ Archilog/src/Diagramme.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
+
+
 
 public class Diagramme {
 
+	private int[][] tab;
+	private ArrayList<Bean> noeuds;
+	public static final int Implement = 2;
+	public static final int Extends = 1;
+
+	public Diagramme(String path) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int[][] getTab() {
+		return tab;
+	}
+
+	public void setTab(int[][] tab) {
+		this.tab = tab;
+	}
+
+	public ArrayList<Bean> getNoeuds() {
+		return noeuds;
+	}
+
+	public void setNoeuds(ArrayList<Bean> noeuds) {
+		this.noeuds = noeuds;
+	}
+
 }
+
