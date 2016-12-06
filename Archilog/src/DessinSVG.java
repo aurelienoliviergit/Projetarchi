cf11d2596205afe42d0598696f64a026ce8d966b
abel <abel@debian>
2016-12-06 16:31:24 +0100

    Classe DessinSVG mis a jour sans style, a connecter avec la classe Bean

--- Archilog/src/DessinSVG.java
+++ Archilog/src/DessinSVG.java
@@ -1,70 +1,102 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Rectangle;
 import java.lang.reflect.Method;
+import java.util.Scanner;
 
 import org.jfree.graphics2d.svg.SVGGraphics2D;
 
-public class DessinSVG implements Dessin{
+public class DessinSVG {
 	
-	protected int nombreElement=0;
+	private SVGGraphics2D graph; // Dessin 2D ou svg du diagramme
 	
-	protected SVGGraphics2D graph;
-	protected int dimensionX ; //dimension du cadre ordonné
-	protected int dimensionY ; //dimension du cadre abcisse
-	
-	
-	
+	public SVGGraphics2D getGraph() {
+		return graph;
+	}
+
+	public void setGraph(SVGGraphics2D graph) {
+		this.graph = graph;
+	}
+
+// Dessine le cadre du dessin du Diagramme 
 	public DessinSVG(int x, int y){
-		this.dimensionX = x;
-		this.dimensionY = y; // 1+nbreAttribut*12px+nbreMethod*12px+3*12px
-		this.graph = new SVGGraphics2D(this.dimensionX, this.dimensionY);
-		this.graph.setPaint(Color.magenta);
-		this.graph.draw(new Rectangle(0, 0, this.dimensionX, this.dimensionY));
+		this.graph = new SVGGraphics2D(x,y);
 	}
 	
 	
-	public void dessinerNomClass(String n){
-		this.nombreElement++;
-		this.graph.drawString(n, this.dimensionX/10,12*(this.nombreElement+1));
+	
+// Dessine l'ensemble des classes	
+	public void dessinerClass(Bean[] b){
+        Scanner scan2 = new Scanner(System.in); //scanner pour demander les dimensions de la classe
+        
+        for(int i = 0; i< b.length; i++){
+        	System.out.println("entré la position voulue de la classe" + b[i].toString());
+        	int x = scan.nextInt(); //position de l'abcisse du point en haut à gauche du rectangle representant la classe b[i]
+        	int y = scan.nextInt(); //position de l'ordonné du point en haut à gauche du rectangle representant la classe b[i]
+        	this.dessinerClass(b[i],x,y); // Dessine la classe b[i] 
+        	
+        	scan2.close();
+        }
+	}
+
+	
+// Dessine une classe 
+	public void dessinerClass(Bean b,int x, int y){
 		
-	}
-
-	public void dessinerAttribut(String f) {
-		this.nombreElement++;
-		this.graph.drawString(f, this.dimensionX/10,12*(this.nombreElement+1));
-	}
-
-	public void dessinerMethode(Method m) {
-		this.nombreElement++;
-		this.graph.drawString(m.getName(), this.dimensionX/10,12*(this.nombreElement+1));
-	}
-
-	@Override
-	public void placercadre() {
-		// TODO Auto-generated method stub
+		int nbreAttribut = b.nbreAttribut(); // nombre d'attribut dans la classe b
+		int nbreMethod = b.nbreMethod(); 	 // nombre de méthodes dans la classe b
 		
-	}
-
-	@Override
-	public void lier(Class a, int[][] t, int i) {
-		// TODO Auto-generated method stub
+		int dimX = b.tailleDuPlusLongMot()+20 ;		  // dimension en abcisse du rectangle de la classe b, on a ajouté 20 par soucis de clarté
+		int dimY= nbreAttribut*12+nbreMethod*12+3*12; // dimension en ordonné du rectangle de la classe b, 12 étant taille du texte
 		
+		this.getGraph().draw(new Rectangle(x, y, dimX , dimY)); // dessin du rectangle
+		
+		int nbrElement = 1; //nbrElement est un repere pour savoir combien d'element on deja ete dessine dans le rectangle
+		this.getGraph().drawString(b.name(), x+10, y +12*(nbrElement)); // ecrire le nom de la methode dans le cadre
+		nbrElement++;
+		
+		// ecrire dans le cadre de la classe b les attributs
+		for(int i = 0; i<nbreAttribut; i++){
+			this.getGraph().drawString(b.getAttribut()[i], x+10, y+12*(nbrElement));
+			nbrElement++;
+		}
+		
+		// ecrire dans le cadre de la classe b les methodes
+		for(int i = 0; i<nbreMethod; i++){
+			this.getGraph().drawString(b.getMethod()[i], x+10, y+12*(nbrElement));
+			nbrElement++;
+		}
 	}
+	
 	
 	public static void main(String[] args) {
-		SVGGraphics2D g2 = new SVGGraphics2D(300, 200);
+		/*SVGGraphics2D g2 = new SVGGraphics2D(300, 200);
 		 g2.setPaint(Color.RED);
 		 g2.draw(new Rectangle(10, 10, 280, 180));
 		 g2.drawString("string",15 , 15);
 		 String svgElement = g2.getSVGElement();
 		
 		 
-		 DessinSVG d = new DessinSVG(500, 500);
+		 DessinSVG d = new DessinSVG(500, 500, Color.BLUE);
 		 d.dessinerNomClass("nom");
 		 d.dessinerAttribut("A1");
 		 d.dessinerAttribut("A2");
+		 
 		 String svg = d.graph.getSVGElement();
 		 System.out.println( svg);
+		 */
+		
+        Scanner scan = new Scanner(System.in); //scanner pour demander les dimensions de la classe
+        System.out.println("Choisir des dimensions cohèrentes avec le diagramme");
+        System.out.println("taille en abcisse du cadre?");
+        int i = scan.nextInt();
+        System.out.println("taille en ordonné du cadre?");
+        int j = scan.nextInt();
+		 
+		DessinSVG d = new DessinSVG(i,j); //Dessine le cadre de taille (i,j)
+		d.dessinerClass(); //on dessine dans d l'ensemble des classes représentées par b qui est du type Bean[]
+		 
+		 
 	}
 
 }
