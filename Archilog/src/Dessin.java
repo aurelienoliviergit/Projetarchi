45ee1fb514beb70672d0c9ebbb5474637e456dc2
ArmandMellet <armand.mellet@hotmail.fr>
2016-12-11 19:57:39 +0100

    Re-ajout des couches de dessin

---
+++ Archilog/src/Dessin.java
@@ -0,0 +1,32 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import coucheBasse.Diagramme;
+
+public interface Dessin {
+
+	int couleur = 0;
+	/*
+	 public void dessinerAttribut(String f);
+	 
+	public default void dessinercadre(Class a){
+
+		   this.placercadre();
+		  Method[]m=a.getDeclaredMethods();
+			for(int i=0;i<m.length ;i++){
+				this.dessinerMethode(m[i]);
+			}
+			Field[]f=a.getFields();
+			for(int i=0;i<m.length ;i++){
+				this.dessinerAttribut(f[i].getName());
+			}
+
+			this.lier(a, null, couleur);
+			
+	}
+	public void dessinerMethode(Method m);
+	public void placercadre();
+	*/
+	public void dessinerClass(Diagramme d);
+	public void lier(Diagramme d);
+	
+}
